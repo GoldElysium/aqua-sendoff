3ac54e7ce109ed8823cd44ced401675f0250f901
GoldElysium <goldelysium@gmail.com>
2024-08-13 17:55:30 +0200

    feat: i18n

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,11 @@
 <script>
+	import { ParaglideJS } from '@inlang/paraglide-sveltekit'
+	import { i18n } from '$lib/i18n'
+
 	import '../app.css';
 </script>
 
-<slot />
+
+<ParaglideJS {i18n}>
+	<slot />
+</ParaglideJS>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,7 @@
+<script>
+	import * as m from '$lib/paraglide/messages.js'
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<p>Your current language: {m.language_test()}</p>
