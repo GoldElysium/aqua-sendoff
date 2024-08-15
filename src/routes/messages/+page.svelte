932035beef3f9ec2f3ecbe8f98ed367a65647784
GoldElysium <goldelysium@gmail.com>
2024-08-15 17:31:38 +0200

    refactor: move submissions to own component

--- src/routes/messages/+page.svelte
+++ src/routes/messages/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
-	import * as m from '$lib/paraglide/messages.js'
 	import type { PageData } from './$types';
+	import FanSubmissions from './FanSubmissions.svelte';
 
 	// Prefilled by server data; assuming the data is sorted by server side code.
 	// Data from the server is autofilled into this variable.
@@ -9,58 +9,9 @@
 </script>
 
 <div class="grid place-items-center">
-	<div class="group max-w-[96rem] py-8">
-		<div class="flex gap-4">
-			<div class="bg-blue-400 px-4 py-2 rounded-md">
-				<input type="checkbox" checked id="show-messages">
-				<label for="show-messages">{m.messages()}</label>
-			</div>
-			<div class="bg-blue-400 px-4 py-2 rounded-md">
-				<input type="checkbox" checked id="show-artwork">
-				<label for="show-artwork">{m.artworks()}</label>
-			</div>
-			<div class="bg-blue-400 px-4 py-2 rounded-md">
-				<input type="checkbox" checked id="show-pictures">
-				<label for="show-pictures">{m.photos()}</label>
-			</div>
-			<div class="bg-blue-400 px-4 py-2 rounded-md">
-				<input type="checkbox" checked id="show-videos">
-				<label for="show-videos">{m.videos()}</label>
-			</div>
-		</div>
-		<div class="grid place-items-center py-8">
-			<div class="grid max-w-[96rem] grid-cols-4 gap-4">
-				{#each data.data as item}
-					<div
-						class="p-4 border border-black rounded-md hidden"
-						class:message={item.message.length > 0}
-						class:artwork={item.images.length > 0 && item.images[0].type === 'image' && item.images[0].subtype === 'artwork'}
-						class:photo={item.images.length > 0 && item.images[0].type === 'image' && item.images[0].subtype === 'picture'}
-						class:video={item.images.length > 0 && item.images[0].type === 'video'}
-					>
-						<code>ID: {item.id}</code>
-						<p>Author: {item.author}</p>
-					</div>
-				{/each}
-			</div>
-		</div>
+	<div class=" max-w-[96rem] py-8">
+		<FanSubmissions data={data.data} />
 	</div>
 </div>
 
-<style lang="postcss">
-	.message {
-		@apply group-[:first-child:has(#show-messages:checked)]:block;
-	}
 
-	.artwork {
-		@apply group-[:first-child:has(#show-artwork:checked)]:block;
-	}
-
-	.photo {
-		@apply group-[:first-child:has(#show-pictures:checked)]:block;
-	}
-
-	.video {
-		@apply group-[:first-child:has(#show-videos:checked)]:block;
-	}
-</style>
