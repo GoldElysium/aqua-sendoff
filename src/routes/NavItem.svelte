5ce95f83f8869a784453dcbe960066b69471008c
Yumeo0 <mail@yumeo.dev>
2024-08-15 23:14:25 +0200

    Color Navbar Text and better temp icon

--- src/routes/NavItem.svelte
+++ src/routes/NavItem.svelte
@@ -1,10 +1,15 @@
 <script>
-    export let href = '';
+	export let href = '';
 </script>
 
-
 <a {href} on:click>
-    <span>
-        <slot />
-    </span>
+	<span>
+		<slot />
+	</span>
 </a>
+
+<style>
+	a {
+		color: #2e3191;
+	}
+</style>
