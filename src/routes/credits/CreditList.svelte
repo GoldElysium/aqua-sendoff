449456c25f38ed6b0f3afc6b896f1de583d4ce64
SMMEGAtive <megative.music@gmail.com>
2024-08-18 18:57:21 +0700

    Partial Decoration: Onion Fishing and Fishing Line

--- src/routes/credits/CreditList.svelte
+++ src/routes/credits/CreditList.svelte
@@ -2,19 +2,36 @@
 	import CreditListMember from './CreditListMember.svelte';
 	import type { CreditGroup } from '$lib/types/types';
 
-    export let credits: CreditGroup[] = [];
+	export let credits: CreditGroup[] = [];
+
+	let isVertical: boolean = false; // Check if vertical view
+
+	let ratio = window.innerWidth / window.innerHeight;
+	if (ratio < 0.5) isVertical = true;
 
 	/*
         Programatically split the credit group for every x Line
     */
-    // Parameters for line counts
-    // - GroupHeaderLine / GroupMemberLine = Value of how many line for header or member, split in case Header weighs bigger than the members
-    // - LineCountMax = Maximum line count for credits. This is soft limit, so groups with 10 or 11 lines may exist
-    // - LineCountHardMax = Hard limit for credits lines. If exceeding this, the new credit group will be split to the next one
-	const groupHeaderLine = 1;
-	const groupMemberLine = 1;
-	const lineCountMax = 10;
-	const lineCountHardMax = 12;
+	// Parameters for line counts
+	// - GroupHeaderLine / GroupMemberLine = Value of how many line for header or member, split in case Header weighs bigger than the members
+	// - LineCountMax = Maximum line count for credits. This is soft limit, so groups with 10 or 11 lines may exist
+	// - LineCountHardMax = Hard limit for credits lines. If exceeding this, the new credit group will be split to the next one
+	let groupHeaderLine;
+	let groupMemberLine;
+	let lineCountMax;
+	let lineCountHardMax;
+
+	if (isVertical) {
+		groupHeaderLine = 1;
+		groupMemberLine = 1;
+		lineCountMax = 8;
+		lineCountHardMax = 10;
+	} else {
+		groupHeaderLine = 1;
+		groupMemberLine = 1;
+		lineCountMax = 10;
+		lineCountHardMax = 12;
+	}
 
 	// Count current lines and storage for the split credit groups
 	let lineCounter = 0;
