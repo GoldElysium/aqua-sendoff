27b8d689250664a9ad09bd647876c680f89826b7
Edqe14 <34704796+Edqe14@users.noreply.github.com>
2024-08-19 22:55:16 +0700

    refactor(credits): fix footer

--- src/routes/credits/CreditFooter.svelte
+++ src/routes/credits/CreditFooter.svelte
@@ -1,12 +1,10 @@
-<footer
-	class="h-full flex flex-col items-center justify-center text-foreground-blue-accent"
-	style="background: linear-gradient(180deg, #ffc387 0%, #dba166 100%)"
->
-	<p class="text-2xl md:text-4xl font-bold flex flex-col grow items-center justify-center py-48">
-		<span>Thank you for the 6 years.</span>
+<footer class="flex flex-col items-center justify-between text-foreground-blue-accent" style="background: linear-gradient(180deg, #ffc387 0%, #dba166 100%)">
+	<p class="text-3xl font-bold flex flex-col grow items-center mt-24">
+		<img src="/assets/cat.svg" alt="cat" class="w-1/2 md:w-auto" />
+		<span class="mt-12">Thank you for the 6 years.</span>
 		<span>We had fun while it lasted, take care!</span>
 	</p>
-	<div class="text-center flex flex-col relative mb-4">
+	<div class="text-center flex flex-col mt-48">
 		<small class="text-xs md:text-sm text-opacity-70 mb-2">
 			This is a fan project, there is no affiliation with hololive and COVER corp
 		</small>
--- src/routes/messages/MessagesFooter.svelte
+++ src/routes/messages/MessagesFooter.svelte
@@ -1,4 +1,4 @@
-<footer class="flex flex-col items-center justify-between" style="color:#39388f">
+<footer class="flex flex-col items-center justify-between text-foreground-blue-accent">
 	<p class="text-3xl font-bold flex flex-col grow items-center mt-24">
 		<img src="/assets/cat.svg" alt="cat" class="w-1/2 md:w-auto" />
 		<span class="mt-12">Thank you for the 6 years.</span>
