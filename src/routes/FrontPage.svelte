f78ba1f659ae217d64372578e27f0c32428d9b81
David Chen <3145205+RiceCakess@users.noreply.github.com>
2024-08-27 21:42:26 -0700

    Add animation to the waves

--- src/routes/FrontPage.svelte
+++ src/routes/FrontPage.svelte
@@ -1,5 +1,6 @@
 <script>
 	import Ship from './Ship.svelte';
+	import Waves from './Waves.svelte';
 </script>
 
 <!-- https://stackoverflow.com/questions/61308575/tailwind-h-screen-doesn-t-work-properly-on-mobile-devices -->
@@ -21,14 +22,7 @@
 			alt="island"
 			class="absolute z-[0] 2xl:bottom-0 -bottom-4 md:right-[10%] -right-4 scale-75 2xl:scale-100"
 		/>
-
-		<img
-			src="/assets/WaveTranslucent.svg"
-			class="w-full z-[7] h-36 object-fit bottom-0 absolute"
-			alt="wave"
-		/>
-		<img src="/assets/Wave.svg" class="w-full object-fit h-36 bottom-0 absolute" alt="wave" />
-
+		<Waves />
 		<slot></slot>
 	</div>
 </div>
---
+++ src/routes/Waves.svelte
@@ -0,0 +1,32 @@
+<div class=" absolute h-36 w-full bottom-0 z-[7] overflow-hidden">
+    <img
+        src="/assets/WaveTranslucent.svg"
+        class="scale-x-[250%] w-full h-full wave-animated"
+        alt="wave"
+    />
+</div>
+<img src="/assets/Wave.svg" class="w-full object-fit h-36 bottom-0 absolute" alt="wave" />
+
+<style>
+	.wave-animated {
+		animation: rock 5s infinite ease-in-out;
+		transform-origin: bottom center;
+	}
+
+    /* Try to match the Ship rocking movement */
+	@keyframes rock {
+        0%,
+		100% {
+            transform: scaleX(250%) translate(0, 0) 
+		}
+		25% {
+			transform: scaleX(250%) translate(-100px, 10px)
+		}
+		50% {
+            transform: scaleX(250%) translate(0, 5px)
+        }
+        75% {
+			transform: scaleX(250%) translate(100px, 10px)
+		}
+	}
+</style>
