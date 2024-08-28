ec1e96282bc3a353bdb26fbccf1171ff072da406
Edqe14 <34704796+Edqe14@users.noreply.github.com>
2024-08-28 19:31:53 +0700

    refactor: dont prefix default language

--- src/lib/i18n.ts
+++ src/lib/i18n.ts
@@ -3,6 +3,6 @@ import { createI18n } from '@inlang/paraglide-sveltekit';
 import * as runtime from '$lib/paraglide/runtime.js';
 
 export const i18n = createI18n(runtime, {
-	prefixDefaultLanguage: 'always',
+	prefixDefaultLanguage: 'never',
 	exclude: ['/assets']
 });
