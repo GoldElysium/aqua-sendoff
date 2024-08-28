8d8443367451858b81af6a0f524f4921d8054fe0
Edqe14 <34704796+Edqe14@users.noreply.github.com>
2024-08-28 22:04:11 +0700

    refactor: match title with i18n

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,28 +1,10 @@
 <script>
 	import { ParaglideJS } from '@inlang/paraglide-sveltekit';
 	import { i18n } from '$lib/i18n';
-	import * as m from '$lib/paraglide/messages';
 
 	import '../app.css';
 	import Header from './Header.svelte';
 </script>
-
-<svelte:head>
-	<meta property="og:title" content={m.title()} />
-	<meta property="og:url" content="https://minatoaqua.ch" />
-	<meta property="og:type" content="website" />
-	<meta property="og:image" content="https://cdn.holoen.fans/hefw/media/aquairo_comp.jpg" />
-	<meta property="twitter:card" content="summary_large_image" />
-	<meta property="theme-color" content="#B9DFF4" />
-	<meta
-		name="description"
-		content={m.description()}
-	/>
-	<meta
-		name="og:description"
-		content={m.description()}
-	/>
-</svelte:head>
 
 <ParaglideJS {i18n}>
 	<Header />
--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -1,6 +1,25 @@
 <script>
+	import * as m from '$lib/paraglide/messages';
 	import Navbar from './Navbar.svelte';
 </script>
+
+<svelte:head>
+	<title>{m.title()}</title>
+	<meta property="og:title" content={m.title()} />
+	<meta property="og:url" content="https://minatoaqua.ch" />
+	<meta property="og:type" content="website" />
+	<meta property="og:image" content="https://cdn.holoen.fans/hefw/media/aquairo_comp.jpg" />
+	<meta property="twitter:card" content="summary_large_image" />
+	<meta property="theme-color" content="#B9DFF4" />
+	<meta
+		name="description"
+		content={m.description()}
+	/>
+	<meta
+		name="og:description"
+		content={m.description()}
+	/>
+</svelte:head>
 
 <header>
 	<Navbar />
