2332f1de3b84c16164210dcfb2451a6d3e8b0ad0
Edqe14 <34704796+Edqe14@users.noreply.github.com>
2024-08-28 12:51:34 +0700

    refactor: button style

--- src/routes/messages/AnchorScroll.svelte
+++ src/routes/messages/AnchorScroll.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import { scrollPos, type ScrollPos } from '$lib/scrollStore';
+	import { ArrowDownOutline, ArrowUpOutline } from 'flowbite-svelte-icons'
 
 	export let targetElement: HTMLElement;
 	export let section: ScrollPos;
@@ -13,8 +14,14 @@
 <!-- The thing that when you click it scrolls to bottom or top of page -->
 <button
 	class="fixed {direction === 'bottom' && 'bottom-4'} {direction === 'top' &&
-		'top-20'} right-8 z-[100] text-white"
+		'top-20'} right-8 z-[100] border rounded flex px-1 pr-2 py-1 hover:bg-white text-foreground-blue-accent bg-white/75 transition-colors"
 	on:click={handleScroll}
 >
-	go to {section}
+	{#if direction === 'top'}
+		<ArrowUpOutline />
+		Go back up
+	{:else}
+		<ArrowDownOutline />
+		Go down
+	{/if}
 </button>
