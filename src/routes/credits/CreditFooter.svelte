d15c4023c9e938d5966d8e9b8989266a78108811
LegendAVRS <61302586+LegendAVRS@users.noreply.github.com>
2024-08-28 07:47:18 +0700

    style: small tweaks

--- src/routes/credits/CreditFooter.svelte
+++ src/routes/credits/CreditFooter.svelte
@@ -1,8 +1,12 @@
 <script lang="ts">
 	import * as m from '$lib/paraglide/messages';
 </script>
-<footer class="flex flex-col items-center justify-between text-foreground-blue-accent" style="background: linear-gradient(180deg, #ffc387 0%, #dba166 100%)">
-	<p class="text-3xl font-bold flex flex-col grow items-center mt-24">
+
+<footer
+	class="flex flex-col items-center justify-between text-foreground-blue-accent"
+	style="background: linear-gradient(180deg, #ffc387 0%, #dba166 100%)"
+>
+	<p class="text-3xl font-bold flex flex-col grow items-center mt-24 text-center px-4">
 		<img src="/assets/cat.svg" alt="cat" class="w-1/2 md:w-auto" />
 		<span class="mt-12">{m.endText0()}</span>
 		<span>{m.endText1()}</span>
--- src/routes/messages/MessagesFooter.svelte
+++ src/routes/messages/MessagesFooter.svelte
@@ -1,8 +1,9 @@
 <script lang="ts">
 	import * as m from '$lib/paraglide/messages';
 </script>
+
 <footer class="flex flex-col items-center justify-between text-foreground-blue-accent">
-	<p class="text-3xl font-bold flex flex-col grow items-center mt-24">
+	<p class="text-3xl font-bold flex flex-col grow items-center mt-24 text-center px-4">
 		<img src="/assets/cat.svg" alt="cat" class="w-1/2 md:w-auto" />
 		<span class="mt-12">{m.endText0()}</span>
 		<span>{m.endText1()}</span>
