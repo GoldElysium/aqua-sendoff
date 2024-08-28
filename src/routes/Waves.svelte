455e6fa093ec4eea78e3e603e57045e92605e8f6
David Chen <3145205+RiceCakess@users.noreply.github.com>
2024-08-27 21:47:03 -0700

    lint

--- src/routes/Waves.svelte
+++ src/routes/Waves.svelte
@@ -1,9 +1,9 @@
 <div class=" absolute h-36 w-full bottom-0 z-[7] overflow-hidden">
-    <img
-        src="/assets/WaveTranslucent.svg"
-        class="scale-x-[250%] w-full h-full wave-animated"
-        alt="wave"
-    />
+	<img
+		src="/assets/WaveTranslucent.svg"
+		class="scale-x-[250%] w-full h-full wave-animated"
+		alt="wave"
+	/>
 </div>
 <img src="/assets/Wave.svg" class="w-full object-fit h-36 bottom-0 absolute" alt="wave" />
 
@@ -13,20 +13,20 @@
 		transform-origin: bottom center;
 	}
 
-    /* Try to match the Ship rocking movement */
+	/* Try to match the Ship rocking movement */
 	@keyframes rock {
-        0%,
+		0%,
 		100% {
-            transform: scaleX(250%) translate(0, 0) 
+			transform: scaleX(250%) translate(0, 0);
 		}
 		25% {
-			transform: scaleX(250%) translate(-100px, 10px)
+			transform: scaleX(250%) translate(-100px, 10px);
 		}
 		50% {
-            transform: scaleX(250%) translate(0, 5px)
-        }
-        75% {
-			transform: scaleX(250%) translate(100px, 10px)
+			transform: scaleX(250%) translate(0, 5px);
+		}
+		75% {
+			transform: scaleX(250%) translate(100px, 10px);
 		}
 	}
 </style>
